b4a39ec0fc316f6b8fd916c031881845bd270a91
111Vivek111 <prajapativivek93165@gmail.com>
2026-07-14 17:23:26 +0530

    auth.js created

---
+++ frontend/js/auth.js
@@ -0,0 +1,33 @@
+function saveToken(token) {
+
+    localStorage.setItem(
+        "token",
+        token
+    );
+}
+
+function getToken() {
+
+    return localStorage.getItem(
+        "token"
+    );
+}
+
+function logout() {
+
+    localStorage.removeItem(
+        "token"
+    );
+
+    window.location.href =
+        "login.html";
+}
+
+function requireLogin() {
+
+    if (!getToken()) {
+
+        window.location.href =
+            "login.html";
+    }
+}
