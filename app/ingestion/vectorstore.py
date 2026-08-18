e9c784d12b626e12e0573b4b07c1ca12db3f06e7
111Vivek111 <prajapativivek93165@gmail.com>
2026-08-18 10:18:24 +0530

    remove build vector store and make load vec

--- app/ingestion/vectorstore.py
+++ app/ingestion/vectorstore.py
@@ -3,7 +3,8 @@ from qdrant_client import QdrantClient
 from langchain_qdrant import QdrantVectorStore
 from app.config import (
     QDRANT_URL,
-    QDRANT_API_KEY
+    QDRANT_API_KEY,
+    QDRANT_COLLECTION_NAME
 )
 def build_vector_store(
     chunks,
@@ -29,6 +30,12 @@ def build_vector_store(
 
 
 
+# app/ingestion/vectorstore.py
+from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance
+from langchain_qdrant import QdrantVectorStore
+
+
 def load_vector_store(embeddings):
 
     client = QdrantClient(
@@ -36,11 +43,20 @@ def load_vector_store(embeddings):
         api_key=QDRANT_API_KEY
     )
 
+    if not client.collection_exists(QDRANT_COLLECTION_NAME):
+
+        vector_size = len(embeddings.embed_query("dimension probe"))
+
+        client.create_collection(
+            collection_name=QDRANT_COLLECTION_NAME,
+            vectors_config=VectorParams(
+                size=vector_size,
+                distance=Distance.COSINE
+            )
+        )
+
     return QdrantVectorStore(
-
         client=client,
-
-        collection_name="policy_manuals",
-
+        collection_name=QDRANT_COLLECTION_NAME,
         embedding=embeddings
     )
